b5d8f8477671bf4b6f241d7cc7fae0d80ae9ce5f
oluwafemi <oo9701s@gre.ac.uk>
2024-12-17 14:52:32 +0000

    Detect Turtle crossing and level up car speed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car = CarManager()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -20,13 +20,19 @@ while game_is_true:
     time.sleep(0.1)
     screen.update()
 
-    car.create_car()
-    car.move_cars()
+    car_manager.create_car()
+    car_manager.move_cars()
 
     #Detect collision with car
-    for car in car.all_cars:
+    for car in car_manager.all_cars:
        if car.distance(player) < 20:
            game_is_true = False
+
+    # #Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+
 
 
 screen.exitonclick()
