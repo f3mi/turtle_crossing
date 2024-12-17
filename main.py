20deedf2a35380d4bd76ab0d057cef08052c5d6d
oluwafemi <oo9701s@gre.ac.uk>
2024-12-17 13:03:09 +0000

    create car behaviour and movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from turtle import Screen
+
+from car_manager import CarManager
 from player import Player
 
 screen = Screen()
@@ -7,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -16,3 +19,6 @@ game_is_true = True
 while game_is_true:
     time.sleep(0.1)
     screen.update()
+
+    car.create_car()
+    car.move_cars()
