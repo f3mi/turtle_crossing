92973d75903cedd6e7e9d6a1346a2eaf3b4ab1d7
oluwafemi <oo9701s@gre.ac.uk>
2024-12-17 15:14:05 +0000

    added scoreboard and game over sequence

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 
 from car_manager import CarManager
 from player import Player
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -27,11 +29,13 @@ while game_is_true:
     for car in car_manager.all_cars:
        if car.distance(player) < 20:
            game_is_true = False
+           scoreboard.game_over()
 
     # #Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 
 
---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.hideturtle()
+        self.penup()
+        self.goto(-200, 250)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+        self.update_scoreboard()
+
+    def level_up(self):
+        self.level += 1
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
